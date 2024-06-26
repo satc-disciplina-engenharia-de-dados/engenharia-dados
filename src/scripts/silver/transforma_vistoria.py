fba4fc98ca1d09d772c2c60baaa08d110eb73355
Arthurdelucahonorato <126838860+Arthurdelucahonorato@users.noreply.github.com>
2024-06-26 00:11:02 -0300

    Update transforma_vistoria.py
    agora esta Ok

--- src/scripts/silver/transforma_vistoria.py
+++ src/scripts/silver/transforma_vistoria.py
@@ -3,4 +3,14 @@ def transformar_vistoria(spark: SparkSession, bronze_path, silver_path):
     df_bronze = spark.read.format("delta").load(bronze_path)
 
     df_silver = df_bronze.select('id_vistoria', 'id_imovel', 'data_vistoria', 'descricao')
-print("Transformacao da tabela vistoria finalizada")
+
+    df_silver.show(5)
+
+    (
+        df_silver
+        .write
+        .format("delta")
+        .mode('overwrite')
+        .save(silver_path)
+    )
+    print("Transformacao da tabela vistoria finalizada")
